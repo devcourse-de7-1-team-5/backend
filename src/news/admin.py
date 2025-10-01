18abef9b2e8aa4c9cd41195ac28b21bf1fe6f374
Seonggil Jeong <sgjeong1108@icloud.com>
2025-10-01 16:24:15 +0900

    feat: impl news 목록 조회 View, open Mock Data 크롤링 api

--- src/news/admin.py
+++ src/news/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import News
+
+
 # Register your models here.
+
+
+class NewsAdmin(admin.ModelAdmin):
+    readonly_fields = ['date', 'press', 'link']
+    fieldsets = [
+        ('뉴스 제목', {'fields': ['title']}),
+        ('뉴스 내용', {'fields': ['content']}),
+        ('작성자', {'fields': ['press']}),
+        ('작성일', {'fields': ['date']}),
+        ('링크', {'fields': ['link']}),
+    ]
+
+    list_display = [
+        'title', 'content', 'press'
+    ]
+
+
+admin.site.register(News, NewsAdmin)
--- src/news/views.py
+++ src/news/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+from .crawler import NaverNewsCrawler
+from .models import News
+
+
 # Create your views here.
+
+
+def index(request):
+    news_list = News.objects.all()
+    return render(request, 'news/index.html', {'news_list': news_list})
+
+
+def crawl(request):
+    crawler = NaverNewsCrawler(
+        query="태양의 후예",
+        ds="2023.06.03",
+        de="2023.06.04"
+    )
+
+    crawler_data = list(crawler)
+    news_models = [News(
+        title=x.title,
+        content=x.content,
+        date=x.date,
+        press=x.press,
+        link=x.link
+    ) for news in crawler_data for x in news]
+
+    # when
+    News.objects.bulk_create(news_models, ignore_conflicts=True)
+    return render(request, 'news/index.html')
+    return None
