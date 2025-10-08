85d008b3c5eda1f941f6810983126ca9f5b5e265
Seonggil Jeong <sgjeong1108@icloud.com>
2025-10-08 15:14:49 +0900

    refactor: 비동기 테스트 추가 및 크롤링 flow 테스트 케이스 추가

--- src/news/tests/test_insert_news_linked_to_drama.py
+++ src/news/tests/test_insert_news_linked_to_drama.py
@@ -1,58 +1,12 @@
 from django.db.models.expressions import Exists, OuterRef
-from django.test.testcases import TestCase
+from django.test import TransactionTestCase
 from django.urls import reverse
 
 from dramas.models import Drama, EpisodeInfo
 from news.models import News
 
 
-class TestInsertNewsLinkedToDrama(TestCase):
-    """
-    Test case for inserting news articles linked to dramas.
-    """
-
-    def test_select_only_dramas_with_episodes(self):
-        """
-        에피소드를 하나 이상 가지고 있는 드라마만 선택하는지 테스트
-        :return:
-        """
-        # when
-        dramas_with_episodes = Drama.objects.annotate(
-            has_episode=Exists(
-                EpisodeInfo.objects.filter(drama_id=OuterRef('pk'))
-            )
-        ).filter(has_episode=True)
-
-        self.assertEqual(dramas_with_episodes.count(), 1)
-
-        for v in dramas_with_episodes:
-            self.assertTrue(v.has_episode)
-            self.assertIsNotNone(v.pk)
-
-    def test_check_inserting_news_to_correct_drama(self):
-        # given
-        url = reverse("set-up-news")
-        dramas_with_episodes = Drama.objects.annotate(  # 에피소드가 있는 드라마 정보만 조회
-            has_episode=Exists(
-                EpisodeInfo.objects.filter(drama_id=OuterRef('pk'))
-            )
-        ).filter(has_episode=True)
-
-        # when
-        response = self.client.get(url)
-        saved_dramas = dict((drama['title'], drama['news_count']) for drama in
-                            response.data['completed_dramas'])
-
-        # then
-        self.assertIsNotNone(url)
-        self.assertTrue(response.status_code, 200)
-        for drama in dramas_with_episodes:
-            ep_ids = drama.episodes.values_list('id', flat=True)
-            self.assertIn(drama.title, saved_dramas.keys())
-
-            news_count = News.objects.filter(drama_ep_id__in=ep_ids).count()
-            self.assertEqual(news_count, saved_dramas[drama.title])
-
+class TestInsertNewsLinkedToDrama(TransactionTestCase):
     def setUp(self):
         self.drama_has_episode = Drama(
             title="태양의 후예",
@@ -94,3 +48,45 @@ class TestInsertNewsLinkedToDrama(TestCase):
         self.drama_ep3.save()
         self.drama_ep2.save()
         self.drama_ep1.save()
+
+    def test_select_only_dramas_with_episodes(self):
+        """
+        에피소드를 하나 이상 가지고 있는 드라마만 선택하는지 테스트
+        """
+        # when
+        dramas_with_episodes = Drama.objects.annotate(
+            has_episode=Exists(
+                EpisodeInfo.objects.filter(drama_id=OuterRef('pk'))
+            )
+        ).filter(has_episode=True)
+
+        self.assertEqual(dramas_with_episodes.count(), 1)
+
+        for v in dramas_with_episodes:
+            self.assertTrue(v.has_episode)
+            self.assertIsNotNone(v.pk)
+
+    def test_check_inserting_news_to_correct_drama(self):
+        # given
+        url = reverse("set-up-news")
+        dramas_with_episodes = Drama.objects.annotate(  # 에피소드가 있는 드라마 정보만 조회
+            has_episode=Exists(
+                EpisodeInfo.objects.filter(drama_id=OuterRef('pk'))
+            )
+        ).filter(has_episode=True)
+
+
+        # when
+        response = self.client.get(url)
+        saved_dramas = dict((drama['title'], drama['news_count']) for drama in
+                            response.data['completed_dramas'])
+
+        # then
+        self.assertIsNotNone(url)
+        self.assertTrue(response.status_code, 200)
+        for drama in dramas_with_episodes:
+            ep_ids = drama.episodes.values_list('id', flat=True)
+            self.assertIn(drama.title, saved_dramas.keys())
+
+            news_count = News.objects.filter(drama_ep_id__in=ep_ids).count()
+            self.assertEqual(news_count, saved_dramas[drama.title])
