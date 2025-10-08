20ebfa32e3550a7496324637de8d4feb864279f5
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-10-08 21:27:22 +0900

    chore: 사용하지 않는 모듈 제거

--- backend/src/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from todo.views import DramaList, DramaDetail
-
-urlpatterns = [
-    path('dramas/', DramaList.as_view(), name='drama-list'),
-    path('dramas/<int:pk>/', DramaDetail.as_view(), name='drama-detail')
-]
