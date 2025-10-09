5325a6dff248dfc865a20ddcdc581ed05b5d2638
nasihyeon <nasihyun77@gmail.com>
2025-10-10 02:11:11 +0900

    feat: 드라마 목록 웹 페이지 수정 , setup_date.py 업데이트

--- src/dramas/management/commands/setup_data.py
+++ src/dramas/management/commands/setup_data.py
@@ -9,27 +9,27 @@ class Command(BaseCommand):
 
         try:
             # 1. 드라마 기본 정보 크롤링
-            self.stdout.write(self.style.NOTICE("\n[1/3] 드라마 기본 정보 크롤링을 시작합니다..."))
+            self.stdout.write(self.style.NOTICE("\n[1/4] 드라마 기본 정보 크롤링을 시작합니다..."))
             call_command('crawl_dramas')
             call_command('crawl_dramas_desc')
             call_command('crawl_dramas_img')
-            self.stdout.write(self.style.SUCCESS("[1/3] 드라마 기본 정보 크롤링 완료!"))
+            self.stdout.write(self.style.SUCCESS("[1/4] 드라마 기본 정보 크롤링 완료!"))
 
             # 2. 드라마 회차 정보 크롤링
-            self.stdout.write(self.style.NOTICE("\n[2/3] 드라마 회차별 상세 정보 크롤링을 시작합니다..."))
+            self.stdout.write(self.style.NOTICE("\n[2/4] 드라마 회차별 상세 정보 크롤링을 시작합니다..."))
             call_command('crawler_episodes')
-            self.stdout.write(self.style.SUCCESS("[2/3] 드라마 회차별 상세 정보 크롤링 완료!"))
+            self.stdout.write(self.style.SUCCESS("[2/4] 드라마 회차별 상세 정보 크롤링 완료!"))
 
             # 4. 장르 크롤링
-            self.stdout.write(self.style.NOTICE("\n[2/3] 드라마 장르 크롤링을 시작합니다..."))
+            self.stdout.write(self.style.NOTICE("\n[3/4] 드라마 장르 크롤링을 시작합니다..."))
             call_command('crawler_genre')
-            self.stdout.write(self.style.SUCCESS("[2/3] 드라마 장르 크롤링 완료!"))
+            self.stdout.write(self.style.SUCCESS("[3/4] 드라마 장르 크롤링 완료!"))
 
 
             # 5. 뉴스 정보 크롤링
-            self.stdout.write(self.style.NOTICE("\n[3/3] 드라마 관련 뉴스 정보 크롤링을 시작합니다..."))
+            self.stdout.write(self.style.NOTICE("\n[4/4] 드라마 관련 뉴스 정보 크롤링을 시작합니다..."))
             call_command('crawl_news')
-            self.stdout.write(self.style.SUCCESS("[3/3] 드라마 관련 뉴스 정보 크롤링 완료!"))
+            self.stdout.write(self.style.SUCCESS("[4/4] 드라마 관련 뉴스 정보 크롤링 완료!"))
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"\n초기 데이터 설정 중 오류가 발생했습니다: {e}"))
