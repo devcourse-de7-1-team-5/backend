3327162a2f046ec05693345b9d91a2dc5aee6f2f
nasihyeon <nasihyun77@gmail.com>
2025-10-10 02:09:02 +0900

    드라마 목록 웹 페이지 수정 + setup_data.py 업데이트

--- src/dramas/management/commands/setup_data.py
+++ src/dramas/management/commands/setup_data.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
 class Command(BaseCommand):
-    help = '드라마, 회차 정보, 뉴스 데이터를 순서대로 크롤링하여 초기 데이터를 설정합니다.'
+    help = '드라마, 회차 정보, 장르, 뉴스 데이터를 순서대로 크롤링하여 초기 데이터를 설정합니다.'
 
     def handle(self, *args, **kwargs):
         self.stdout.write(self.style.SUCCESS("===== 초기 데이터 설정을 시작합니다. ====="))
@@ -11,6 +11,8 @@ class Command(BaseCommand):
             # 1. 드라마 기본 정보 크롤링
             self.stdout.write(self.style.NOTICE("\n[1/3] 드라마 기본 정보 크롤링을 시작합니다..."))
             call_command('crawl_dramas')
+            call_command('crawl_dramas_desc')
+            call_command('crawl_dramas_img')
             self.stdout.write(self.style.SUCCESS("[1/3] 드라마 기본 정보 크롤링 완료!"))
 
             # 2. 드라마 회차 정보 크롤링
@@ -18,7 +20,13 @@ class Command(BaseCommand):
             call_command('crawler_episodes')
             self.stdout.write(self.style.SUCCESS("[2/3] 드라마 회차별 상세 정보 크롤링 완료!"))
 
-            # 3. 뉴스 정보 크롤링
+            # 4. 장르 크롤링
+            self.stdout.write(self.style.NOTICE("\n[2/3] 드라마 장르 크롤링을 시작합니다..."))
+            call_command('crawler_genre')
+            self.stdout.write(self.style.SUCCESS("[2/3] 드라마 장르 크롤링 완료!"))
+
+
+            # 5. 뉴스 정보 크롤링
             self.stdout.write(self.style.NOTICE("\n[3/3] 드라마 관련 뉴스 정보 크롤링을 시작합니다..."))
             call_command('crawl_news')
             self.stdout.write(self.style.SUCCESS("[3/3] 드라마 관련 뉴스 정보 크롤링 완료!"))
